8db1c79177b920310e6137d11bed0b4db99e34eb
brickfaced <brickfazed@hotmail.com>
2019-02-03 19:15:02 -0800

    Fixed import statements for new file structure

--- pages/project.js
+++ pages/project.js
@@ -1,10 +1,10 @@
-import Layout from '../comps/MyLayout.js'
+import Layout from '../components/MyLayout.js'
 import { withRouter } from 'next/router'
 
 export default withRouter((props) => (
   <Layout>
     <h1>{props.router.query.title}</h1>
-    <div className='markdown'>
+    <div className=''>
       Project Page
     </div>
     <style jsx global>{`
